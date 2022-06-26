2ee07dc0c8e029d721126c0851209066a71174e7
osangu <osangu012@gmail.com>
2022-06-26 17:39:52 +0900

    ADD :: github scrawl

--- main.py
+++ main.py
@@ -79,3 +79,60 @@ class Scrawler:
 
         return job_amount_list
 
+    @property
+    def repository_amount_list(self):
+        language_list = self._list_editor.change_for_usage('encode', 'github', self.language_list)
+        repository_amount_list = []
+
+        for language in language_list:
+            request = get(f'https://github.com/search?l={language}&q={language}&type=Repositories').text
+
+            soup = str(Bs(request, 'lxml').find('body').find_all('h3')[2])
+            start = re.match('<h3>', soup).span()[1]
+            finish = soup.find(' repository results')
+
+            repository_amount_list.append(
+                int(soup[start:finish].translate(
+                    str.maketrans('<span class="v-align-middle"> Showing,available\n',
+                                  ' ' * len('<span class="v-align-middle"> Showing,available\n'))
+                ).replace(' ', ''))
+            )
+            sleep(8)
+
+        return repository_amount_list
+
+    @property
+    def issue_amount_list(self):
+        language_list = self._list_editor.change_for_usage('encode', 'github', self.language_list)
+        print(len(language_list), language_list)
+        issue_amount_list = []
+        for language in language_list:
+            request = get(f'https://github.com/search?l={language}&q={language}&type=Issues').text
+
+            soup = str(Bs(request, 'lxml').find('body').find_all('h3')[-1]).translate(
+                str.maketrans('<h3></h3>,issues \n', ' ' * len('<h3></h3>,issues \n')))
+
+            issue_amount_list.append(int(soup.replace(' ', '')))
+            sleep(8)
+
+        return issue_amount_list
+
+    @property
+    def commit_amount_list(self):
+        language_list = self._list_editor.change_for_usage('encode', 'github', self.language_list)
+        commit_amount_list = []
+        for language in language_list:
+            request = get(f'https://github.com/search?l={language}&q={language}&type=commits').text
+
+            soup = str(Bs(request, 'lxml').find('body').find_all('h3')[-1])
+
+            start = soup.find('Showing') + 8
+            finish = soup.find(' available')
+
+            commit_amount_list.append(
+                int(soup[start:finish].replace(',',''))
+            )
+
+            sleep(8)
+
+        return commit_amount_list
